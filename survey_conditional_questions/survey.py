d33a04943aba03b2d7582ca94bb9a29fd81ad4cd
Genaro Zorrilla <ccarly@users.noreply.github.com>
2017-06-15 06:54:46 -0500

    Actualiza función get_list_questions
    Mejora en la busqueda e inicia las respuestas de no complen con la condicion

--- survey_conditional_questions/survey.py
+++ survey_conditional_questions/survey.py
@@ -5,6 +5,7 @@
 ##############################################################################
 from openerp import fields, models
 import logging
+import random
 
 _logger = logging.getLogger(__name__)
 
@@ -24,11 +25,21 @@ class survey_question(models.Model):
         copy=False,
         help="In order to edit this field you should first save the question"
     )
+    random_show = fields.Boolean(
+        'Random Show',
+        copy=False,
+        # we add copy = false to avoid wrong link on survey copy,
+        # should be improoved
+    )
     answer_id = fields.Many2one(
         'survey.label',
         'Answer',
         copy=False,
     )
+
+    _defaults = {
+        'random_show': False,
+    }
 
     def validate_question(
             self, cr, uid, question, post, answer_tag, context=None):
@@ -54,31 +65,38 @@ class survey_question(models.Model):
                     return {}
                 else:
                     return checker(cr, uid, question, post, answer_tag, context=context)
-
+                
+    def get_random_label(self, cr, uid, ids, label=False, context=None):
+        question = self.browse(cr, uid, ids, context=context)
+        labels_ids = label and question.labels_ids_2 or question.labels_ids
+        if not question.random_show:
+            return labels_ids 
+        if not labels_ids:
+            return labels_ids
+        return random.sample(labels_ids, len(labels_ids))
+        
 
 class survey_user_input(models.Model):
     _inherit = 'survey.user_input'
 
     def get_list_questions(self, cr, uid, survey, user_input_id):
 
-        obj_questions = self.pool['survey.question']
-        obj_user_input_line = self.pool['survey.user_input_line']
         questions_to_hide = []
+        obj_questions = self.pool['survey.question']
         question_ids = obj_questions.search(
             cr,
             uid,
-            [('survey_id', '=', survey.id)])
+            [('survey_id', '=', survey.id),('conditional', '=', True)])
         for question in obj_questions.browse(cr, uid, question_ids):
-            if question.conditional:
-                for question2 in obj_questions.browse(cr, uid, question_ids):
-                    if question2 == question.question_conditional_id:
-                        input_answer_id = obj_user_input_line.search(
-                            cr,
-                            uid,
-                            [('user_input_id', '=', user_input_id),
-                             ('question_id', '=', question2.id)])
-                        for answers in obj_user_input_line.browse( cr,uid,input_answer_id):
-                            value_suggested = answers.value_suggested
-                            if question.answer_id != value_suggested:
-                                questions_to_hide.append(question.id)
+            if question.question_conditional_id:
+                for answers in question.question_conditional_id.user_input_line_ids:
+                    if answers.user_input_id.id == user_input_id:
+                        value_suggested = answers.value_suggested
+                        if question.answer_id != value_suggested:
+                            questions_to_hide.append(question.id)
+                            # Init answer
+                            for answers1 in question.user_input_line_ids:
+                                answers1.value_suggested = None
         return questions_to_hide
+
+
